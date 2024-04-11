4a06b2d8d6614b8287c3e3bec6101ce7d36f1051
Henry <koberidiculous@gmail.com>
2024-04-11 17:16:16 +0800

    調整個人圖片-2024-04-11

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -19,7 +19,7 @@
           p 過去接觸視覺設計、網頁及手機APP設計、響應式(RWD)網站設計、政府網站設計等多重項目，能獨立從規劃流程、設計草稿到最後精稿的產出，具備設計介面的互動能力，以及0-1建置設計系統。在職場過程中，我深知學習經驗和反饋的重要性，從最初的平面設計、UI/UX再到前端語法切版，每個階段都必須持續學習並不斷精進自己的技能，才能在科技產業中與時俱進。
           p 為提升自身實力，透過自學及side project的方式培養獨立思辨，在職同時，加入UI實戰營課程，以直播互動課程、作業及小組專題，不斷吸收業界新知與實戰技能，持續學習並接受挑戰。我期待將各種知識與經驗相互融合，實現並創造更有影響力的作品，為社會注入更多正面與美好的力量。
           p 在興趣方面，我喜歡文創手作、旅行，並對觀察生活細節充滿好奇心，享受從不同領域的新知中獲取靈感，期待以此為催化劑，帶來更多積極而有益的改變。
-          p.text-right.q-pt-xl 
+          p.text-right
             q-icon.q-mx-sm(name="far fa-envelope" color="#585858") 
             | xlingfun@gmail.com
   .banner-divider
@@ -108,29 +108,27 @@ export default defineComponent({
   }
   .img-container {
     position: relative;
-    padding: 0px 120px;
+    display: flex;
+    justify-content: center;
     @media (min-width: 1440px) and (max-width: 1920px) {
       top: calc(50px * (100vw * (1440 / 1920) / 1920px));
-    }
-    @media (max-width: 1440px) {
-      display: flex;
-      justify-content: center;
     }
     .img {
       width: 100%;
       position: relative;
-      margin-top: 7vw;
+      margin-top: 2vw;
       border-radius: 273px;
+      z-index: -2;
       @include rwd.large {
-        max-width: 500px;
-        margin-top: 135px;
+        width: 500px;
+        margin-top: 75px;
       }
-      @include rwd.medium {
-        max-width: 500px;
+      @media (min-width: 1440px) and (max-width: 1920px) {
+        width: 500px;
       }
       @media (max-width: 1440px) {
         border-radius: 0px;
-        min-width: 300px;
+        max-width: 500px;
       }
     }
     .intro01 {
@@ -188,5 +186,6 @@ export default defineComponent({
   width: 100%;
   position: absolute;
   top: 460px;
+  z-index: -1;
 }
 </style>
